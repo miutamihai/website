9cc572fb6a3e7fe48096feefb2891301a1454cca
mihaimiuta <mihaimiuta@proton.me>
2024-02-15 17:16:58 +0200

    [master] Add example of markdown conversion

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module website
 go 1.22.0
 
 require github.com/a-h/templ v0.2.543
+
+require github.com/yuin/goldmark v1.7.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"github.com/yuin/goldmark"
 	"os"
 )
 
 func main() {
+	markdownBytes, err := os.ReadFile("test.md")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var buf bytes.Buffer
+	if err := goldmark.Convert(markdownBytes, &buf); err != nil {
+		panic(err)
+	}
+
+	os.WriteFile("test.html", buf.Bytes(), 0644)
+
 	component := hello("John")
 	component.Render(context.Background(), os.Stdout)
 }
