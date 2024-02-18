bd58aca45286483000754748722dd6b7eda2a252
mihaimiuta <mihaimiuta@proton.me>
2024-02-18 23:27:35 +0200

    [master] Add initial page building

--- posts/build_pages.go
+++ posts/build_pages.go
@@ -2,22 +2,74 @@ package posts
 
 import (
 	"bytes"
+	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/yuin/goldmark"
 )
 
-func Make() {
-	markdownBytes, err := os.ReadFile("posts/2024/02-stop-switching-to-vscode.md")
+const postsPath = "./posts"
+const outputPath = "./out"
+
+func getMarkdownFiles() []string {
+	var files []string
+
+	err := filepath.Walk(postsPath, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && filepath.Ext(path) == ".md" {
+			files = append(files, path)
+		}
+
+		return nil
+	})
 
 	if err != nil {
 		panic(err)
 	}
 
-	var buf bytes.Buffer
-	if err := goldmark.Convert(markdownBytes, &buf); err != nil {
-		panic(err)
-	}
+	return files
+}
 
-	os.WriteFile("out/test.html", buf.Bytes(), 0644)
+func makeNewPath(path string) string {
+	trimmedPath := strings.Split(path, ".md")[0]
+
+	return fmt.Sprintf("%s/%s.html", outputPath, trimmedPath)
+}
+
+func Make() {
+	files := getMarkdownFiles()
+
+	for _, file := range files {
+		markdownBytes, err := os.ReadFile(file)
+
+		if err != nil {
+			panic(err)
+		}
+
+		var buf bytes.Buffer
+		if err := goldmark.Convert(markdownBytes, &buf); err != nil {
+			panic(err)
+		}
+
+		newPath := makeNewPath(file)
+
+		directoryCreationErr := os.MkdirAll(filepath.Dir(newPath), 0770)
+
+		if directoryCreationErr != nil {
+			panic(err)
+		}
+		_, fileCreationErr := os.Create(newPath)
+
+		if fileCreationErr != nil {
+			panic(fileCreationErr)
+		}
+
+		os.WriteFile(newPath, buf.Bytes(), 0644)
+	}
 }
