e1c46d591b2f2df01021d85696b92a97f1eda19c
mihaimiuta <mihaimiuta@proton.me>
2024-03-21 14:19:32 +0200

    [master] Add import route for xgo

--- cmd/builder/main.go
+++ cmd/builder/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"website/internal/posts"
 	"website/internal/tailwind"
 	"website/internal/views"
+
+	"github.com/a-h/templ"
 )
+
+type importInput struct {
+	component templ.Component
+	name      string
+}
+
+func makeGoImports() []importInput {
+	return []importInput{
+		{
+			component: views.XgoGoImport(),
+			name:      "xgo",
+		},
+	}
+}
 
 func main() {
 	if err := tailwind.Ensure(); err != nil {
@@ -23,4 +40,14 @@ func main() {
 	home := views.Home()
 
 	home.Render(context.Background(), indexFile)
+
+	for _, goImport := range makeGoImports() {
+		file, err := os.Create(fmt.Sprintf("out/%s.html", goImport.name))
+
+		if err != nil {
+			panic(err)
+		}
+
+		goImport.component.Render(context.Background(), file)
+	}
 }
